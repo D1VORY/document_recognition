5213ec988c94d0c86e2c2594d4ae93beb7681403
Olexandr Dubchak <a.dubchak@disoft.dev>
2022-05-28 19:28:09 +0300

    done building graph with json

--- core/logic/graph.py
+++ core/logic/graph.py
@@ -1,6 +1,8 @@
+import itertools
 
 
 class Node:
+    newid = itertools.count()
     TOP = 'TOP'
     BOTTOM = 'BOTTOM'
     RIGHT = 'RIGHT'
@@ -10,17 +12,40 @@ class Node:
     BOTTOM_RIGHT = 'BOTTOM_RIGHT'
     BOTTOM_LEFT = 'BOTTOM_LEFT'
 
-    def __init__(self, data):
-        self.data = data
+    class NodeType:
+        ANCHOR = 'anchor'
+        VALUE = 'value'
+        DEFAULT = 'default'
+
+    def __init__(self, value='', name='unkown', node_type=NodeType.DEFAULT, node_id: int=None):
+        self.value = value
+        self.name = name
+        self.node_type = node_type
+        self.id = node_id or next(self.newid)
+
         self.right_node = None
         self.left_node = None
         self.top_node = None
         self.bottom_node = None
-
         self.top_right_node = None
         self.top_left_node = None
         self.bottom_right_node = None
         self.bottom_left_node = None
+
+    @classmethod
+    def get_reversed_position(cls, position):
+        reversed = {
+            cls.TOP: cls.BOTTOM,
+            cls.BOTTOM: cls.TOP,
+            cls.RIGHT: cls.LEFT,
+            cls.LEFT: cls.RIGHT,
+            cls.TOP_RIGHT: cls.BOTTOM_LEFT,
+            cls.TOP_LEFT: cls.BOTTOM_RIGHT,
+            cls.BOTTOM_LEFT: cls.TOP_RIGHT,
+            cls.BOTTOM_RIGHT: cls.TOP_LEFT,
+        }
+        return reversed.get(position)
+
 
     def get_corresponding_attr(self, position: str):
         if position == self.RIGHT:
@@ -59,16 +84,32 @@ class Node:
             self.bottom_right_node = node
         return self.get_corresponding_attr(position)
 
+    def set_relation(self, other_node, position):
+        if not self.get_corresponding_attr(position):
+            self.set_to_corresponding_attr(position, other_node)
+            reversed_position = self.get_reversed_position(position)
+            other_node.set_to_corresponding_attr(reversed_position, self)
+
 
 class DocumentGraph:
     def __init__(self, nodes=None):
         self.nodes = nodes or []
+        self._nodes_ids = {node.id: index for index, node in zip(range(len(self.nodes)), self.nodes)}
 
     def add_to_graph(self, node: Node):
+        if node.id in self._nodes_ids:
+            raise ValueError('THIS ID ALREADY PRESENT')
         self.nodes.append(node)
+        self._nodes_ids[node.id] = len(self.nodes) - 1
 
     def remove_from_graph(self, node: Node):
         pass
+
+    def get_node_by_id(self, id:int):
+        index = self._nodes_ids.get(id)
+        if index is None:
+            return None
+        return self.nodes[index]
 
     def find_corresponding_node(self, node, graph2):
         pass
--- core/logic/parse_template.py
+++ core/logic/parse_template.py
@@ -1,6 +1,32 @@
+import json
+
+from core.logic.graph import DocumentGraph, Node
 
 
 class JSONTemplate:
     def __init__(self, data: dict):
-        self.languages = dict.get('language')
+        self.languages = data.get('language')
         self.fields = data.get('fields')
+        self.graph = DocumentGraph()
+
+    def build_graph(self):
+        for field in self.fields:
+            node = Node(
+                node_id=field.get('id'),
+                node_type=field.get('type'),
+                name=field.get('name'),
+            )
+            self.graph.add_to_graph(node)
+        for field, node in zip(self.fields, self.graph.nodes):
+            for position in field.get('position', []):
+                position_direction = position.get('pos')
+                other_node = self.graph.get_node_by_id(position.get('dep_id'))
+                node.set_relation(other_node, position_direction)
+
+
+
+dct = json.load(open('v3.json'))
+tplt = JSONTemplate(dct)
+tplt.build_graph()
+
+print('IM FUCKING UMMING')
